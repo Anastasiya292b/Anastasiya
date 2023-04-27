30a583631ffb75a8f46a0f1688fa54c7610f1194
Anastasiya292b <131886682+Anastasiya292b@users.noreply.github.com>
2023-04-27 07:54:59 +0300

    Файлы из предыдущих лабораторных работ

---
+++ function/independent/Фаил.js
@@ -0,0 +1,22 @@
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
+
+const distance = 210;
+        const time = 17;
+        const speed1 = 80;
+        const speed2 = 80;
+alert(
+    "между автомобилями " + (distance - time * (speed1 + speed2)) + "км"
+);
+
+const edge=2;
+    console.log (edge**2);
+    let surfaceSquare=6*edge**2;
+    console.log(surfaceSquare);
+    console.log(edge**3);
---
+++ function/practical/Задание.js
@@ -0,0 +1,4 @@
+const someValue = 45;
+{ 
+ console.log(typeof someValue === "number" || typeof someValue === "bigint");
+}
